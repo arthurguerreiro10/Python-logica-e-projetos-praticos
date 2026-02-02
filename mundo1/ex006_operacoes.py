e287918643c5194677522a9353dc6d0980772922
Arthur Guerreiro <arthurguerreiro36@gmail.com>
2026-02-02 00:25:20 -0300

    Create ex006_operacoes.py

---
+++ mundo1/ex006_operacoes.py
@@ -0,0 +1,9 @@
+numero = int(input('Digite um numero: '))
+
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero **(1/2)
+
+print(f'O dobro de número {numero} é {dobro}')
+print(f'O triplo de número {numero} é {triplo}')
+print(f'A raiz quadrada de número {numero} é {raiz}')
